d6231535549b99854f70a2a63b3abc1755058ef8
Yashar Najafi <148863523+yashar2028@users.noreply.github.com>
2024-04-20 21:48:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__) #here we define the app
-app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///Book.sqlite3'
-db = SQLALchemy(app) #We set a database object
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Book.db'
+db = SQLAlchemy(app)
 
 
 class Book(db.Model):
-    """""
-    Here I defined a model where I store objects to represent: Book is the model and id, title, author are rows
-    """
-    id = db.column("id", db.integer, primarykey=True)
-    title = db.column(db.String(50))
-    author = db.column(db.String(50))
-    def __int__(self, title, author):
-        self.title = title
-        self.author = author
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable = False)
+    author = db.Column(db.String(50), nullable=False)
 
-def addcontext():
-    with app.app_context():
-    db.create_all()
+    def __repr__(self):
+        return f'<Book {self.id}'
 
 
-
-@app.route('/book') #I created a path here that can be accessed by /book. This shows the list of items stored in the database for Book. I used the model html file as exact as it was shown in lectures
+@app.route('/')
 def book():
-    render_template("index.html", Book=Book.query.all())
+    db.create_all()
+    books = Book.query.all()
+    return render_template("index.html", Book=books)
 
 
-@app.route('/add_book', methods=["GET","POST"])
+@app.route('/add_book', methods=['GET', 'POST'])
 def submit_book():
+    if request.method == "POST":
+        author = request.form['author']
+        title = request.form['title']
+        new_book = Book(title=title, author=author)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('book'))
     return render_template("submit.html")
 
 
 if __name__ == '__main__':
-    create_db()
-    app.run(port=5001, debug=True)
+    app.run(debug=True)
